25cba460fcb850e9c60efd0ba4a34324c5a99d86
jose-almir <jr.cod.dev@gmail.com>
2025-03-17 16:50:00 -0300

    feat: transition between pages

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,11 +1,26 @@
+import { useRouter } from "next/router";
 import { Footer } from "../Footer";
 import { Navbar } from "../Navbar";
+import { AnimatePresence, motion } from "framer-motion";
 
 export function Layout({ children }) {
+  const router = useRouter();
   return (
     <>
       <Navbar />
-      <main>{children}</main>
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={router.route}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{
+            duration: 0.5,
+          }}
+        >
+          <main>{children}</main>
+        </motion.div>
+      </AnimatePresence>
       <Footer />
     </>
   );
