b389e0d62315376a948d993fb710f51cdc043fcb
jose-almir <jr.cod.dev@gmail.com>
2025-03-23 14:13:13 -0300

    chore: SEO improvements canonical url

--- components/Seo/index.js
+++ components/Seo/index.js
@@ -1,5 +1,11 @@
 import { useI18n } from "@/hooks/useI18n";
 import Head from "next/head";
+import { useRouter } from "next/router";
+
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://arantia.art"
+    : "http://localhost:3000";
 
 export default function Seo({
   title = "Arântia",
@@ -7,14 +13,16 @@ export default function Seo({
   image = "https://arantia.art/banner-main.png",
 }) {
   const { t, lang } = useI18n({});
+  const router = useRouter();
+  const canonicalUrl = `${baseUrl}${router.asPath}`;
 
   return (
     <Head>
       <title>{title}</title>
-      <meta name="title" content={title}/>
+      <meta name="title" content={title} />
       <meta name="keywords" content={t("keywords")} />
       <meta name="author" content="Arântia" />
-      <meta name="description" content={description || t("description")}/>
+      <meta name="description" content={description || t("description")} />
       <meta property="og:title" content={title} />
       <meta
         property="og:description"
@@ -23,6 +31,7 @@ export default function Seo({
       <meta property="og:image" content={image} />
       <meta property="og:type" content="website" />
       <meta property="og:locale" content={lang} />
+      <meta property="og:url" content={canonicalUrl}/>
       <meta property="og:site_name" content="Arântia" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:image" content={image} />
@@ -31,6 +40,7 @@ export default function Seo({
         name="twitter:description"
         content={description || t("description")}
       />
+      <meta name="twitter:card" content="summary_large_image"/>
       <link
         rel="icon"
         type="image/png"
@@ -45,6 +55,20 @@ export default function Seo({
         href="/apple-touch-icon.png"
       />
       <link rel="manifest" href="/site.webmanifest" />
+      <link rel="canonical" href={canonicalUrl} />
+      {["pt", "en", "es"].map((l) => (
+        <link
+          key={l}
+          rel="alternate"
+          href={`${baseUrl}/${l}${router.asPath.replace(/^\/[a-z]{2}/, "")}`}
+          hrefLang={l}
+        />
+      ))}
+      <link
+        rel="alternate"
+        href={`${baseUrl}/pt${router.asPath.replace(/^\/[a-z]{2}/, "")}`}
+        hrefLang="x-default"
+      />
     </Head>
   );
 }
